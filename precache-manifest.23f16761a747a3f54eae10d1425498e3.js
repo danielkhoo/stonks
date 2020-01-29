f9159feb954175f2d4754519e20632c2828d7f24
Daniel Khoo <danieljkhoo@gmail.com>
2020-01-29 18:17:46 +0800

    Updates

---
+++ precache-manifest.23f16761a747a3f54eae10d1425498e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a690e2a316d78e94085caf8fde752f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "641a4137dddf24b9401e",
+    "url": "/static/css/main.279bf5b6.chunk.css"
+  },
+  {
+    "revision": "af308fde4043cd38a72e",
+    "url": "/static/js/2.18bbf1eb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.18bbf1eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "641a4137dddf24b9401e",
+    "url": "/static/js/main.47521168.chunk.js"
+  },
+  {
+    "revision": "f9b71e1d66c63f197530",
+    "url": "/static/js/runtime-main.57e9775a.js"
+  }
+]);
